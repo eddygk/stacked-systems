a7b17dd8a61e33c198520d94608775da57bb30e6
Eddy <832737+eddygk@users.noreply.github.com>
2025-05-08 11:52:46 -0400

    Add Jekyll plugins directory and placeholder

---
+++ _plugins/jekyll_plugins.rb
@@ -0,0 +1,8 @@
+# This is a placeholder file to ensure Jekyll loads all necessary plugins
+# GitHub Pages will automatically load the allowed plugins, this is just here
+# to ensure the directory structure exists
+
+# For local development, you may need to add your plugins here
+require 'jekyll-feed'
+require 'jekyll-seo-tag'
+require 'jekyll-sitemap'
